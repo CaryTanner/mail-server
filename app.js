1c764fdf3b273db31817470299f4a12229cfc32b
CaryTanner <cary.landon@gmail.com>
2020-12-14 11:26:38 +0100

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 require('dotenv').config()
 const hbs = require("nodemailer-express-handlebars");
+const cors = require('cors')
 
 import { userController, mailingController } from './controller';
 let PORT = process.env.PORT || 8080
@@ -13,6 +14,7 @@ const app = express();
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 // controllers here
 app.use('/', userController);
