4240e632f31655f6d491206ecb5d1b9f9deae256
CaryTanner <cary.landon@gmail.com>
2020-12-15 12:43:25 +0100

    attachment for email

--- mailing/index.js
+++ mailing/index.js
@@ -73,18 +73,36 @@ const mailOptionsToNewContact = {
     from: `Cary Tanner <${SENDER_EMAIL_ADDRESS}>`,
     to: data.email,
     subject: 'Contact from Cary Tanner- Web Developer',
-   
-    html: `<p>Hi ${data.name},</p>
+    attachments: [{
+      filename: 'tannerLogo.jpg',
+      path: __dirname + '/tannerLogo.jpg',
+      cid: 'tannerLogo' //same cid value as in the html img src
+  }],
+    html: `<p style=" font-size:24px">Hi ${data.name},</p>
 
-    <p>Thanks for getting in touch!</p>
+    <p style="font-size:24px">Thanks for getting in touch!</p>
     
-    <p>This is an auto-reply but I will get back to you ASAP.</p>
+    <p style="font-size:24px">This is an auto-reply but I will get back to you ASAP.
     
-    <p>All the best,</p>
+    <p style="font-size:24px">All the best,</p>
+  
+    <p style="font-size:24px">Cary </p>
+
+    <img src="cid:tannerLogo">
+
+    <p style="font-size: 18px;"> Cary Tanner 
+    <br/>
+    <span style="font-size: 14px">Web Developer</span>
+    <br/>
+    <span style="font-size: 14px">Tel. +46 707 27 82 61</span> 
+<br/>
+    <a style="text-decoration:none;font-size: 14px " href="https://carytanner.com">carytanner.com</a>
+    <br/>
+    <a style="text-decoration:none;font-size: 14px " href="https://www.linkedin.com/in/carytanner/">LinkedIn</a>
+    <br/>
+    <a style="text-decoration:none; font-size: 14px" href="https://github.com/CaryTanner">GitHub</a></p>
+   
     
-    <p>Cary Tanner<p>
-    
-    <p> Web Developer- <a href="https://carytanner.com">carytanner.com</a></p> 
     `
 }
 
@@ -92,11 +110,8 @@ const mailOptionsToCary = {
   from: `Cary Tanner <${SENDER_EMAIL_ADDRESS}>`,
   to: SENDER_EMAIL_ADDRESS,
   subject: 'you have an new contact msg',
-  // template: 'contactReply.hbs',
-  // context: {
-  //   name: data.name
-  // }
-  html: `<h1> hey Cary</h1>
+ 
+  html: `<h1> Hey Cary</h1>
   <h1> Someone wants to contact you: <h1>
   <ul><li> From: ${data.name} </li> 
   <li> email from: ${data.email} </li> 
